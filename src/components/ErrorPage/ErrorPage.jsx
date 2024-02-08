1ac25e04acac049406e1f4bc212169af53b0ee6c
mitanvir05 <tanvir.cse05@gmail.com>
2024-02-08 13:21:17 +0600

    Header footer added

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1>Found Error !!!!!!!!</h1>
+            <Link to ="/">Back Home</Link>
+        </div>
+    );
+};
+
+export default ErrorPage;
