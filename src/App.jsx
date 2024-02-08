77e7d265fa86bccef99d5024b66df3eac8cdb044
mitanvir05 <tanvir.cse05@gmail.com>
2024-02-08 22:41:42 +0600

    banner added

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-        <h1>Vite + React</h1>
+        <h1>Job Search React App</h1>
     </>
   )
 }
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,8 +1,16 @@
 const Banner = () => {
     return (
-        <div>
-            <h1 className="text-5xl text-center">Banner Page</h1>
+        <div className="hero min-h-screen ">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src="https://static.vecteezy.com/system/resources/previews/011/401/368/original/job-search-logo-vector.jpg" className="max-w-sm rounded-lg " />
+                <div>
+                    <h1 className="text-5xl font-bold">One Step Closer To Your Dream Jobs</h1>
+                    <p className="py-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, cum.</p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+            </div>
         </div>
+
     );
 };
 
